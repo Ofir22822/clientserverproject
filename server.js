38a649b973b57d7a927ddc8771afe09cc9c912e8
Ofir22822 <45202333+Ofir22822@users.noreply.github.com>
2020-12-25 11:18:26 +0200

    up

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
-var http = require('http');
+var http = require('https');
 var url = require('url');
 var fs = require('fs');
 var path = require('path');
 var nodemailer = require('nodemailer');
+
+const GoogleRecaptcha = require('google-recaptcha')
+const googleRecaptcha = new GoogleRecaptcha({secret: 'RECAPTCHA_SECRET_KEY'})
 
 var port = process.env.PORT || 8080;
 var emailAdmin = 'ofir.rahamim@e.braude.ac.il';     //******change username
@@ -58,6 +61,20 @@ app.get('/recapche', function (req, res) {
 
 app.post('/recapche', function (req, res) {  
     res.send(grecaptcha.getResponse(req.param('recapche')));
+
+    // Some pseudo server code:
+    http.on('POST', (request, response) => {
+        const recaptchaResponse = request.body['g-recaptcha-response']
+    
+        googleRecaptcha.verify({response: recaptchaResponse}, (error) => {
+        if (error) {
+            return response.send({isHuman: false})
+        }
+    
+        return response.send({isHuman: true})
+        })
+    })
+
 })
 
 app.get('/getAllData', function (req, res) {  
