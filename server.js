40b4bc1d2176b409b93b4ed95842abcc8ed836e4
Ofir22822 <45202333+Ofir22822@users.noreply.github.com>
2020-12-28 13:11:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var path = require('path');
 var nodemailer = require('nodemailer');
 
 const GoogleRecaptcha = require('google-recaptcha')
-const googleRecaptcha = new GoogleRecaptcha({secret: 'RECAPTCHA_SECRET_KEY'})
+const googleRecaptcha = new GoogleRecaptcha({secret: '6Le8GRQaAAAAAF0pX_Rp7IaErjS4GSR_MkaPEeRx'})
 
 var port = process.env.PORT || 8080;
 var emailAdmin = 'ofir.rahamim@e.braude.ac.il';     //******change username
