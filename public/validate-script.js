4add093798330f014dbfbfec6fc7695f744e028a
Ofir22822 <45202333+Ofir22822@users.noreply.github.com>
2021-02-02 09:53:20 +0200

    Update validate-script.js

--- public/validate-script.js
+++ public/validate-script.js
@@ -409,3 +409,28 @@ function contactSubmitCheck() {
     }
 
 }
+
+function scaleCaptcha(elementWidth) {
+  // Width of the reCAPTCHA element, in pixels
+  var reCaptchaWidth = 304;
+  // Get the containing element's width
+	var containerWidth = $('.container').width();
+  
+  // Only scale the reCAPTCHA if it won't fit
+  // inside the container
+  if(reCaptchaWidth > containerWidth) {
+    // Calculate the scale
+    var captchaScale = containerWidth / reCaptchaWidth;
+    // Apply the transformation
+    $('.g-recaptcha').css({
+      'transform':'scale('+captchaScale+')'
+    });
+  }
+}
+
+$(function() { 
+ 
+  scaleCaptcha();
+  $(window).resize( $.throttle( 100, scaleCaptcha ) );
+  
+});
